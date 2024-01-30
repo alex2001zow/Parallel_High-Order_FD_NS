df153ddfb857f942a3663040d91f401735b47ea5
alex2001zow <47718762+alex2001zow@users.noreply.github.com>
2024-01-30 13:18:46 +0100

    Add files via upload
    Setup block-distribution using OpenMPI's built-in functions. Next step is to find the rank of the corners. Then find the indices of the values that need to be distributed between blocks.

---
+++ code/clean_compile_run.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+EXEC="parallel_solver_mpi"
+
+# Number of dimensions
+NUM_DIM=2
+
+# Number of processes
+NUM_PROCESSES=4
+
+# Grid size
+M_DIM=4
+N_DIM=$M_DIM
+K_DIM=$M_DIM
+
+# Number of processors in each dimension
+M_PROCESSORS=2
+N_PROCESSORS=2
+K_PROCESSORS=1
+
+# Clean previous build
+make clean
+
+# Compile the program
+make all
+
+if [ $? -eq 0 ]; then
+  # Compilation successful, run the program
+  mpirun -np $NUM_PROCESSES ./$EXEC $NUM_DIM $M_DIM $N_DIM $M_PROCESSORS $N_PROCESSORS
+else
+  # Compilation failed
+  echo "Compilation failed."
+fi
